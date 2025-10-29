f3dc60793662b20640eb037d58aff515086afe76
huncholane <huncho@hygo.ai>
2025-10-29 03:31:40 -0700

    refactor: django compiler hides system library files and shows the test where the error comes from

--- compiler/django.lua
+++ compiler/django.lua
@@ -1,14 +1,20 @@
 vim.cmd [[
 set efm=
+
+"Snag the django test error itself
+set efm+=%EERROR:\ %o\ (%m)
+
 "Ignore the system path, add more system paths for python if needed
-"set efm+=%-G%.%#miniconda%.%#
+set efm+=%-G%.%#miniconda%.%#
 
 "Extract the error message
 set efm+=%E%.%#File\ \"%f\"%*[^l]line\ %l%.%#
 set efm+=%-Z%*[-]
-set efm+=%C%.%#Errno\ %n]\ %m
+set efm+=%+C%\\w%.%#
 set efm+=%-C%.%#
 
 "Ignore any leftover text without any error messages
 set efm+=%-G%.%#
+set efm+=%-G%.%.%#
+set efm+=%-G%p
 ]]
