628eeb66386b6cb139b5305a08ccfb6b3358b3a8
huncholane <huncho@hygo.ai>
2025-10-28 22:36:33 -0700

    refactor: django compiler hides extra text and uses the error message at very end of test error

--- compiler/django.lua
+++ compiler/django.lua
@@ -1,15 +1,14 @@
 vim.cmd [[
+set efm=
 "Ignore the system path, add more system paths for python if needed
-set efm=%-G%.%#miniconda%.%#
-
-"Ignore the squiggly lines underneath error messages
-set efm+=%-G%p%*[~^]
+"set efm+=%-G%.%#miniconda%.%#
 
 "Extract the error message
-set efm+=%E%*[\ ]File\ \"%f\"%*[^l]line\ %l%.%#
-set efm+=%C%p%m
-set efm+=%Z%.%#
+set efm+=%E%.%#File\ \"%f\"%*[^l]line\ %l%.%#
+set efm+=%-Z%*[-]
+set efm+=%C%.%#Errno\ %n]\ %m
+set efm+=%-C%.%#
 
-"Ignore any leftover blank lines
-set efm+=%-G%p
+"Ignore any leftover text without any error messages
+set efm+=%-G%.%#
 ]]
