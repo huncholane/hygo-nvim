ca2ccb5e4d1f5e0c2e6f86c3868f59e4e8b16c64
huncholane <huncho@hygo.ai>
2025-10-29 20:50:47 -0700

    style: move signature help to top right

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -1,5 +1,14 @@
 ---@type LazySpec
 return {
   "ray-x/lsp_signature.nvim",
-  opts = {}
+  opts = {
+    hint_enable = false,
+    handler_opts = {
+      border = "single"
+    },
+    toggle_key = "<C-k>",
+    floating_window_above_cur_line = false,
+    floating_window_off_x = 1000,
+    floating_window_off_y = -1000,
+  }
 }
