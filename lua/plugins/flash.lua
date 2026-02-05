0fe9ecb584b0b749e512c5620a09be9c32f5c85e
huncholane <huncho@hygo.ai>
2026-02-04 21:06:55 -0800

    refactor(flash): s is normal search S is treesitter

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
   opts = {},
   keys = {
     {
-      "S",
+      "s",
       mode = { "n", "x", "o" },
       function()
         require("flash").jump()
@@ -12,7 +12,7 @@ return {
       desc = "Flash",
     },
     {
-      "s",
+      "S",
       mode = { "n", "x", "o" },
       function()
         require("flash").treesitter()
