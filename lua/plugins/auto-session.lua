b9fc974c2fa0da41afbe4ba4f143bf5351ff7391
huncholane <huncho@hygo.ai>
2026-02-07 09:57:10 -0800

    feat: delete session with leader sd

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -3,8 +3,22 @@ vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,
 ---@type LazySpec
 return {
   "rmagatti/auto-session",
+  dependencies = { "folke/which-key.nvim" },
   lazy = false,
-  opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "~/Leet", "/tmp" },
-  }
+  config = function()
+    local session = require("auto-session")
+    session.setup({
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "~/Leet", "/tmp" },
+    })
+
+    -- Wait for other plugins for keymaps to register with which key
+    vim.defer_fn(function()
+      vim.keymap.set("n", "<leader>s", "", { desc = "Sessions" })
+      -- Delete session and exit
+      vim.keymap.set("n", "<leader>sd", function()
+        session.delete_session()
+        vim.cmd(":wqa!")
+      end, { desc = "Delete Session" })
+    end, 100)
+  end,
 }
