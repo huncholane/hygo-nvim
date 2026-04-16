126c1d94ee660989b336031ba3057d9cdf477798
huncholane <huncho@hygo.ai>
2026-04-16 15:31:41 -0700

    feat: current blame line for gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,7 +5,13 @@ return {
   event = "VeryLazy",
   config = function()
     local gitsigns = require("gitsigns")
-    gitsigns.setup()
+    gitsigns.setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 300,
+        virt_text_pos = "eol",
+      },
+    })
     require("extensions.git-extensions").setup()
   end,
 }
