0084d535b7fe4e53657e2b951485461e7dcafa88
huncholane <huncho@hygo.ai>
2026-04-17 20:00:56 -0700

    fix: easy align

--- lua/plugins/easy_align.lua
+++ lua/plugins/easy_align.lua
@@ -3,7 +3,8 @@ return {
   "junegunn/vim-easy-align",
   config = function()
     vim.g.easy_align_delimiters = {
-      ["c"] = { pattern = [[\(#\|//\|--\)]], ignore_groups = {} },
+      ["c"] = { pattern = [[\(#\|//\|--\) ]], ignore_groups = {}, right_margin = 0 },
+      ["="] = { pattern = [[= ]], ignore_groups = {}, right_margin = 0 },
     }
     vim.cmd([[
 " Start interactive EasyAlign in visual mode (e.g. vipga)
